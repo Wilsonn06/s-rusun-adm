e7e6eaff949ba08d5cb115cd892e996c2e77ae30
Wilsonn06 <161280166+Wilsonn06@users.noreply.github.com>
2025-11-21 13:28:43 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'mysql.srusun.id',
-  user: 'wilson',
-  password: 'WilsonSRusunDB!2025',
-  database: 'payment',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
