46ed59eaab51987c3c992918567c7301c1ac588c
Wilsonn06 <161280166+Wilsonn06@users.noreply.github.com>
2025-11-27 23:25:02 +0700

    Update floor.js

--- services/floor.js
+++ services/floor.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const db = require('../db'); 
+
 
 router.get('/tower/:tower_id', async (req, res) => {
   const { tower_id } = req.params;
