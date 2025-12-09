3f9fe4e7e04242848e545add4de43605aa9c779f
Wilsonn06 <wilsonraymundus@gmail.com>
2025-12-09 10:41:50 +0700

    Tes CI/CD

--- services/flat.js
+++ services/flat.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
-
+//
 router.get('/', async (req, res) => {
   try {
     const [rows] = await db.query(
